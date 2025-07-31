7029a4b0073fa8b7acf13917eacc2aa007c4e1ff
sarveshsalokhe26 <sarveshsalokhe26@gmail.com>
2025-08-01 00:28:09 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,26 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+//Asking flutter to run the base widget of the application
 void main() {
   runApp(MyApp());
 }
 
+//Defing the Function MyApp in which we will handle tthe change notifier
 class MyApp extends StatelessWidget {
+  //constructing letting flutter know that it is an immutable data
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Returning the change notifier
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
+      create: (context) =>
+          MyAppState(), //Change notifier provider provides the changes that happened in the MyAppState
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black12),
         ),
         home: MyHomePage(),
       ),
@@ -24,20 +29,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//Writing the function where we keep on getting the random words from the user
 class MyAppState extends ChangeNotifier {
+  //Creating a variable which stores the random word pairs from the english word packages
   var current = WordPair.random();
 }
 
+//writing the home page
 class MyHomePage extends StatelessWidget {
+  //constructor (for immutable data lts flutter konow the changes that occur in the data so that it can render the changed data)
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Writing the function which whatches the chages that occur in MyAppState
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
       body: Column(
-        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+        children: [
+          Text('I have a random Fucking wordPair in my mind dawg ;'),
+          Text(appState.current.asLowerCase),
+        ],
       ),
     );
   }
